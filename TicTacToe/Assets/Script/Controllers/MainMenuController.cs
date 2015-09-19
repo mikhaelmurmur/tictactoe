4a19e5d50796263ab4a04f0744e8fdc35008b739
Mikhail Stolovich <misha@develops.today>
2015-09-19 10:03:59 +0300

    Added menu added menu scene added newgame (one-way) added options menu (one-way) ----------------------------- added build for pc started building for pc because of simple and fast building and checking gameitself

---
+++ TicTacToe/Assets/Script/Controllers/MainMenuController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+
+public class MainMenuController : MonoBehaviour
+{
+    [SerializeField]
+    GameObject mainMenu, optionsMenu;
+    public void NewGame()
+    {
+        Application.LoadLevel("GameCore");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void ShowOptions()
+    {
+        mainMenu.transform.DOMoveX(-5000, 3f);
+
+        optionsMenu.transform.DOMoveX(Camera.main.pixelWidth/2 + 50, 2f);
+    }
+}
